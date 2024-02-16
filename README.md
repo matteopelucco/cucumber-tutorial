bfc4cf38c566e647966b5e73100bed5f070d3d0e
Matteo Pelucco <matteo.pelucco@gmail.com>
2024-02-16 23:15:00 +0100

    First Cucumber Test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cucumber-tutorial
+
+mvn archetype:generate -DarchetypeGroupId=io.cucumber -DarchetypeArtifactId=cucumber-archetype -DarchetypeVersion=7.15.0 -DgroupId=hellocucumber -DartifactId=hellocucumber -Dpackage=hellocucumber -Dversion=1.0.0-SNAPSHOT -DinteractiveMode=false
---
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,53 @@
+package hellocucumber;
+
+import io.cucumber.java.en.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StepDefinitions {
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+    static class IsItFriday {
+        static String isItFriday(String today) {
+            return "Friday".equals(today) ? "TGIF" : "Nope";
+        }
+    }
+
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Friday")
+    public void today_is_friday() {
+        today = "Friday";
+    }
+
+    @Given("today is Sunday")
+    public void today_is_sunday() {
+        today = "Sunday";
+    }
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+
+
+
+}
